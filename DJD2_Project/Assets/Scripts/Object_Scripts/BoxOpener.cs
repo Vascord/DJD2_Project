912dd279183fc2da12561689ee9d289d5cb092dc
LizardonPT <joaofilipegoncalves2000@gmail.com>
2020-12-11 15:30:02 +0000

    Room 2 add light puzzle

---
+++ DJD2_Project/Assets/Scripts/Object_Scripts/BoxOpener.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxOpener : MonoBehaviour
+{
+    [SerializeField] GameObject Light_1;
+    [SerializeField] GameObject Light_2;
+    [SerializeField] GameObject Light_3;
+
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Light_1.GetComponent<Light>().enabled == true &&
+            Light_2.GetComponent<Light>().enabled == true &&
+            Light_3.GetComponent<Light>().enabled == true)
+        {
+            animator.SetTrigger("Interact");
+        }
+    }
+}
