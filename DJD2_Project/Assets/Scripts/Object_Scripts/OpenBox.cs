edef12f38ba98620c00bb5384eea7ae370fef023
Vasco Rodrigues Duarte <duarte-vasco@outlook.com>
2020-12-13 18:28:09 +0000

    Add pattern puzzle, need organization

---
+++ DJD2_Project/Assets/Scripts/Object_Scripts/OpenBox.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenBox : MonoBehaviour
+{
+    [SerializeField] GameObject[] paternCubes;
+    [SerializeField] GameObject[] cubes;
+    private int e;
+    private int i;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        e = 0;
+        for(i = 0; i < 4; i++)
+        {
+            if(paternCubes[i].GetComponent<Renderer>().material.color == 
+                cubes[i].GetComponent<Renderer>().material.color)
+            {
+                e++;
+            }
+        }
+
+        Debug.Log(e);
+
+        if(e == 4)
+        {
+            gameObject.GetComponent<Animator>().SetTrigger("Activate");
+        }
+
+        
+    }
+}
---
+++ DJD2_Project/Assets/Scripts/Object_Scripts/OpenDoorDetector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoorDetector : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] Animator[] doors;
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider player)
+    {
+        if(player.transform.CompareTag("Player")==true) {
+            foreach(Animator door in doors)
+            {
+                door.SetTrigger("Interact");
+            }
+            Destroy(gameObject);
+        }
+    }
+}
