d4fd1e7ec81a573083289614d70ce3dbd8b28c0d
Vasco Rodrigues Duarte <duarte-vasco@outlook.com>
2020-12-17 02:32:19 +0000

    Add Last Modifications

---
+++ DJD2_Project/Assets/Scripts/Object_Scripts/ReturnMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnMenu : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Cursor.lockState = CursorLockMode.None;
+        if (other.gameObject.tag == "Player")
+            SceneManager.LoadScene(0);
+    }
+}
