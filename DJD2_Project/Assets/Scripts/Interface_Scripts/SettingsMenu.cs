3f237570c20bc8574e9ac055efdd6bbdd9071321
Vasco Rodrigues Duarte <duarte-vasco@outlook.com>
2021-01-28 03:35:23 +0000

    Fix resulotions in build game

--- DJD2_Project/Assets/Scripts/Interface_Scripts/SettingsMenu.cs
+++ DJD2_Project/Assets/Scripts/Interface_Scripts/SettingsMenu.cs
@@ -34,7 +34,8 @@ public class SettingsMenu : MonoBehaviour
 
         for(int i = 0; i < resolutions.Length; i++)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
+            string option = resolutions[i].width + "x" + resolutions[i].height
+                + " Hz " +  + resolutions[i].refreshRate;
             options.Add(option);
 
             if(resolutions[i].width == Screen.currentResolution.width &&
